6095ca0ae12e0e2be5b5878e06babac12869b0b2
Igor Shevchenko <bnzone@gmail.com>
2021-03-22 14:20:50 -0400

    add personalized info

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,10 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Hey, I am Igor{' '}
+            <span role="img" aria-label="hand wawing emoji">
+              👋🏻
+            </span>
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
