f9737e4da9b04777f4362057f600e639174eb661
Igor Shevchenko <bnzone@gmail.com>
2021-03-26 21:55:21 -0400

    resize the box snd remove extra box from project page

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,13 @@ export async function getStaticProps() {
 export default function Home({ posts }) {
   useEffect(() => {
     var scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xffffff)
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     var renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setSize(400, 400)
     document.querySelector('#model').appendChild(renderer.domElement)
     var geometry = new THREE.BoxGeometry(1, 1, 1)
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    var material = new THREE.MeshBasicMaterial({ color: 0x000000 })
     var cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
     camera.position.z = 5
